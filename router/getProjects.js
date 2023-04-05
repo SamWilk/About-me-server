193822d6f7f228d85e95d1cb4ac73e2de2d34171
SamWilk <samwilk18@yahoo.com>
2023-04-04 22:59:10 -0400

    Updating endpoint

--- router/getProjects.js
+++ router/getProjects.js
@@ -6,13 +6,21 @@ const db = require("../config/config");
 const router = express.Router();
 
 router.get("/githubprojects", async (req, res) => {
-  const GetDate = fs
+  const GetProjects = fs
     .readFileSync(path.join(__dirname, "../sql/GetProjects.sql"))
     .toString();
-  await db.query(GetDate, (err, result) => {
+  db.query(GetProjects, (err, result) => {
     if (err) {
       throw err;
     } else {
+      const GetLogs = fs
+        .readFileSync(path.join(__dirname, "../sql/CreateGithubLog.sql"))
+        .toString();
+      db.query(GetLogs, (err, result) => {
+        console.log("Creating Log");
+        if (err) throw err;
+        console.log("Log Saved");
+      });
       res.status(200).send(result.rows);
     }
   });
---
+++ sql/CreateGithubLog.sql
@@ -0,0 +1,5 @@
+Insert into githublogs (api_name, call_date)
+values (
+    'Github-Projects',
+    CURRENT_TIMESTAMP AT TIME ZONE 'UTC'
+)
